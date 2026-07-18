2cdc825b428d37b96b87a0ae52fa755d8f8bde9d
samuraj198 (Daniil Rakhimzyanov) <daniil885@mail.ru>
2026-07-18 10:50:24 +0400

    fix https

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         Vite::prefetch(concurrency: 3);
     }
 
